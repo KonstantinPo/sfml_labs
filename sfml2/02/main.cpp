75cfc3d4f306e27dfb0529d1fb65b690dacc6e29
Konstantin Pososhnov <pososhnov@gmail.com>
2021-11-10 23:58:32 +0300

    Add cmake project

---
+++ sfml2/02/main.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <cstdint>
+#include <cmath>
+
+static const float BALL_SIZE = 40;
+static const uint32_t WINDOW_WIDTH = 800;
+static const uint32_t WINDOW_HEIGHT = 600;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Wave Moving ball");
+    sf::Clock clock;
+    sf::Vector2f position(10, 350);
+
+    sf::CircleShape ball(BALL_SIZE);
+    ball.setFillColor(sf::Color(0xff, 0xff, 0xff));
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        const auto speedX = 100.0f;
+        const auto amplY = 80.0f;
+        const auto period = 2;
+
+        auto dt = clock.getElapsedTime().asSeconds();
+        sf::Vector2f offset(dt * speedX, amplY * std::sin(dt * 2.0f * (float)M_PI / period));
+        ball.setPosition(offset);
+        window.clear();
+        window.draw(ball);
+        window.display();
+    }
+}
